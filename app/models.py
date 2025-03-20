d79d8ab6edda1a6c56edcbb9d23815ecbc4adf7f
J0hnMilt0n <mjohnmilton41@outlook.com>
2025-03-20 09:05:52 +0530

    user, items have been created

--- app/models.py
+++ app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class UserDetail(models.Model):
+    username = models.CharField(max_length=30)
+    email = models.EmailField()
+    phone = models.IntegerField(max_length=10)
+
+class Item(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.TextField(max_length=200)
+    Ingredients = models.TextField(max_length=100)
